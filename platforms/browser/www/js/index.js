ff11f378a6380c03439c618ffb47fc91b613dbc1
Grzegorz Malinowski <gmalinowski@protonmail.com>
2020-03-27 20:47:43 +0100

    layout

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -18,28 +18,75 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady().bind(this), false);
     },
+
+    timerId: null,
 
     // deviceready Event Handler
     //
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
+    onDeviceReady: function () {
+        this.setSliders();
+        this.setButtons.bind(this)();
     },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+    setSliders: () => {
+        document.getElementById('ss').addEventListener('input', (e) => {
+            document.getElementById('s').innerText = e.target.value;
+        }, false);
+        document.getElementById('sm').addEventListener('input', e => {
+            document.getElementById('m').innerText = e.target.value;
+        });
+        document.getElementById('sh').addEventListener('input', e => {
+            document.getElementById('h').innerText = e.target.value;
+            document.getElementById('shh').MaterialSlider.change(0);
+        });
+        document.getElementById('shh').addEventListener('input', e => {
+            let h = document.getElementById('h');
+            h.innerText = +e.target.value + 100;
+            document.getElementById('sh').MaterialSlider.change(100);
+        });
+    },
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    setButtons: function () {
+        document.getElementById('play').addEventListener('touchend', () => {
+            this.toggleTimer();
+        }, false);
+        document.getElementById('stop').addEventListener('touchend', () => {
+            this.resetTimer();
+        }, false);
+    },
 
-        console.log('Received Event: ' + id);
+    resetTimer: () => {
+        clearInterval(this.timerId);
+        this.timerId = null;
+        document.getElementById('play').children[0].innerText = 'play_arrow';
+        document.getElementById('s').innerText = document.getElementById('ss').value;
+        document.getElementById('m').innerText = document.getElementById('sm').value;
+        document.getElementById('h').innerText = document.getElementById('sh').value;
+    },
+
+    toggleTimer: () => {
+        if (this.timerId) {
+            clearInterval(this.timerId);
+            this.timerId = null;
+            document.getElementById('play').children[0].innerText = 'play_arrow';
+        } else {
+            document.getElementById('play').children[0].innerText = 'pause';
+            let s = document.getElementById('s');
+            let m = document.getElementById('m');
+            let h = document.getElementById('h');
+            this.timerId = setInterval(() => {
+                let time = +s.innerText + +m.innerText * 60 + +h.innerText * 60 * 60;
+                time = time - 1;
+                h.innerText = Math.floor(time / 3600);
+                m.innerText = Math.floor((time % 3600) / 60);
+                s.innerText = ((time % 3600) % 60);
+            }, 1000);
+        }
     }
 };
 
